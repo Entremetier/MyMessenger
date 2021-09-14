810316d55229408f3570230c70485ae5a690a099
Marco Happe <ITN247448@qualifizierung.at>
2021-09-14 15:40:59 +0200

    implemented version controll in App.xaml.cs

--- MyMessenger1/MyMessenger1/App.xaml.cs
+++ MyMessenger1/MyMessenger1/App.xaml.cs
@@ -8,11 +8,43 @@ namespace MyMessenger1
     {
         public const string MessageListKey = "MessageList";
 
+        public const string VersionKey = "version";
+
+        //Die aktuelle Version
+        public const string Version = "1.0.0";
+
         public App()
         {
             InitializeComponent();
 
+            CheckVersion();
+
             MainPage = new NavigationPage(new MainPage());
+        }
+
+        private void CheckVersion()
+        {
+            if (Application.Current.Properties.ContainsKey(VersionKey))
+            {
+                //Prüfen ob es die aktuelle Version ist
+                string oldVersion = Application.Current.Properties[App.VersionKey] as string;
+                if (oldVersion != Version)
+                {
+                    //Hier kann man z.B. Datenbank-Upgrades machen, abhängig von der alten Version
+                    Application.Current.Properties.Clear();
+
+                    //Aktuelle Version schreiben
+                    Application.Current.Properties[App.VersionKey] = App.Version;
+                }
+            }
+            else
+            {
+                //Von meiner aktuellen Applikation, die Eigenschaft (der VersionKey) ist gleich die Version der Klasse App
+                Application.Current.Properties[App.VersionKey] = App.Version;
+
+                //Im Speicher speichern
+                Application.Current.SavePropertiesAsync();
+            }
         }
 
         protected override void OnStart()
